94dc250eb2305d4f8b2de3bee44cf4a7d67ca01b
efcampoy <eliseofernandezcampoy@gmail.com>
2024-11-04 17:36:21 +0100

    Update instance_web.tf

--- terraform/instance_web.tf
+++ terraform/instance_web.tf
@@ -45,16 +45,15 @@ resource "aws_instance" "building_web" {
 
   user_data = <<-EOF
                 #!/bin/bash
-                mkdir -p /home/ubuntu/.ssh
-                echo "$(cat terraform/building_web.key.pub)" >> /home/ubuntu/.ssh/authorized_keys
-                chmod 600 /home/ubuntu/.ssh/authorized_keys
-                chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys
+                sudo mkdir -p /home/ubuntu/.ssh
+                echo "cat building_web.key.pub" >> /home/ubuntu/.ssh/authorized_keys
+                sudo chmod 600 /home/ubuntu/.ssh/authorized_keys
+                sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys
                 sudo apt update
                 sudo apt install nginx -y
                 sudo systemctl enable nginx
                 sudo mkdir -p /var/www/html
                 EOF
-
 
 
   tags = {
